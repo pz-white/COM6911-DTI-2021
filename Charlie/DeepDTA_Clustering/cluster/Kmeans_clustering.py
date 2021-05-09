eb42568b8a18414f97af573bd9903d316020b8b7
Skye-Jiang <69766451+Skye-Jiang@users.noreply.github.com>
2021-05-09 12:53:55 +0100

    Update Kmeans_clustering.py

--- Charlie/DeepDTA_Clustering/cluster/Kmeans_clustering.py
+++ Charlie/DeepDTA_Clustering/cluster/Kmeans_clustering.py
@@ -1,6 +1,7 @@
 from embedding import Embedding
 from sklearn.cluster import KMeans
 import torch
+import time
 
 
 class Kmeans:
@@ -11,28 +12,34 @@ class Kmeans:
         self.num_of_clusters = num_of_clusters
 
     def cluster(self):
+        drug_ids_data = self.data.get_data()[['Drug_ID', 'Drug']]
+        #         unique_drug_ids_data = drug_ids_data.drop_duplicates().reset_index(drop=True)
 
-        drug_ids_data = self.data[['Drug_ID', 'Drug']]
-        unique_drug_ids_data = drug_ids_data.drop_duplicates().reset_index(drop=True)
+        drugid_index_mapping = {drugid: drug_ids_data.index[drug_ids_data['Drug_ID'] == drugid].tolist()
+                                for drugid in drug_ids_data.Drug_ID}
 
-        drugid_index_mapping = {drugid: self.data.index[self.data['Drug_ID'] == drugid].tolist()
-                                for drugid in unique_drug_ids_data.Drug_ID}
+        drug_ids_data = Embedding(drug_ids_data, 'fp').perform_embedding()
 
-        if 'fp' == self.embedding:
-            data = Embedding.fp_embedding(self, unique_drug_ids_data)
-        elif 'mol2vec' == self.embedding:
-            data = Embedding.mol2vec_embedding(self, unique_drug_ids_data)
-        else:
-            data = Embedding.morgan_fp_embedding(self, unique_drug_ids_data)
+        st = time.time()
+        data = torch.tensor(list(drug_ids_data['Drug_vector']))
 
-        data = torch.tensor(list(data['Drug_vector']))
         clustering = KMeans(n_clusters=self.num_of_clusters)
         clustering.fit(data)
 
-        clusters = clustering.labels_
-        data['Cluster'] = '' # may not need this.
+        et = time.time()
+        print("Time required for clustering:", et - st)
+
+        # Map the drug ids to cluster and save it in a dict
+        drug_ids_cluster_map = dict(zip(drug_ids_data.Drug_ID, clustering.labels_))
+
+        # Fetch entire data and add cluster column
+        data = self.data.get_data()
+        data['Cluster'] = ''
+
+        # map the clusters to entire data
         for i, (k, v) in enumerate(drugid_index_mapping.items()):
+            cluster_val = drug_ids_cluster_map.get(k)
             for ind in v:
-                data['Cluster'][ind] = clusters[i]
+                data['Cluster'][ind] = cluster_val
 
         return data
